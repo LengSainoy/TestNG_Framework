d200e127b93897950d431ecde0ac0a6ee0db195e
LengSainoy <78459080+LengSainoy@users.noreply.github.com>
2021-06-26 15:46:16 -0400

    Final

---
+++ src/test/java/TestCases/AdminTest_Base.java
@@ -0,0 +1,25 @@
+package TestCases;
+
+import MyUtil.ConfigurationReader;
+import MyUtil.Driver;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+public class AdminTest_Base {
+     final WebDriver driver = Driver.getDriver();
+   @BeforeClass
+   public void init() {
+      driver.get(ConfigurationReader.getProperty("admin_url"));
+      driver.manage().window().maximize();
+      driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+      driver.manage().deleteAllCookies();
+   }
+
+   @AfterClass
+   public void tearDown() {
+      driver.close();
+   }
+}
